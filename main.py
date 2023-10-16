5d08e1d39f8652ed7bafdf900cdffe4af391f3b1
charleyoshi ʕ •ᴥ•ʔ <charleyoshi@charleyoshis-MacBook-Pro.local>
2023-10-16 12:09:39 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import time
+# selenium 4
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+driver.get("https://bestbet.bet/")
+driver.switch_to.frame(driver.find_element(By.TAG_NAME, "iframe"))
+
+try:
+    betslip = driver.find_element(By.XPATH, "//div[@class='load-betslip-wrapper  ng-isolate-scope']//form/input")
+    betslip.send_keys("CC43496")
+    print(betslip)
+except NoSuchElementException as e:
+    print("1. No such Elements")
+
+try:
+    button = driver.find_element(By.XPATH, "//div[@class='load-betslip-wrapper  ng-isolate-scope']//form/button")
+    button.click()
+except NoSuchElementException as e:
+    print("button: No such Elements")
+
+
+time.sleep(50)
+driver.close()
+driver.quit()
+print('done')
+
+
+# CC43496
+# xpath:
+# //div[contains(@class, 'load-betslip-content')]/form/input
+ 
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Bet Slip Scraper
+
+Bet Slip Scraper is a Python Application to scrape bet slips across multiple betting sites.
+
+## Installation
+Todo
+
+
+## Usage
+Todo
+
+## Collaborators
+@charleyoshi
+@iambee-jay
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+Todo
+[MIT](https://choosealicense.com/licenses/mit/)
